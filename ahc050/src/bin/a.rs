5703525e3ffa93a52852626510b607ee490142f2
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2025-07-06 20:11:45 +0900

    [ahc050]tools のスコア計算を借用

---
+++ ahc050/src/bin/a.rs
@@ -0,0 +1,217 @@
+use ac_library::*;
+use itertools::*;
+use proconio::{input, marker::Chars};
+use rand::Rng;
+use std::time::{Duration, Instant};
+
+pub const N: usize = 40;
+
+#[derive(Clone)]
+struct Info {
+    start_time: Instant,
+    time_limit: Duration,
+    best_answer: (usize, Vec<char>), // 最適解
+}
+
+impl Info {
+    fn new() -> Self {
+        Self {
+            time_limit: Duration::from_millis(1960),
+            start_time: Instant::now(),
+            best_answer: (0, vec![]),
+        }
+    }
+
+    fn is_time_up(&self) -> bool {
+        self.start_time.elapsed() >= self.time_limit
+    }
+
+    fn update_best_answer(&mut self, state: &State) -> bool {
+        todo!()
+    }
+}
+
+#[derive(Clone)]
+pub struct Input {
+    n: usize,          // n=40
+    m: usize,          //
+    s: Vec<Vec<char>>, // 初期盤面
+}
+
+impl Input {
+    fn from_stdin() -> Self {
+        input! {
+            n: usize,
+            m: usize,
+            s: [Chars; n],
+        }
+
+        Self { n, m, s }
+    }
+}
+
+#[derive(Clone)]
+pub struct FieldState {
+    pub s: Vec<Vec<char>>,
+    pub prob: Vec<Vec<f64>>,
+}
+
+#[derive(Clone)]
+struct State {
+    field: FieldState,
+    // ターンごとの生存確率
+    lives: Vec<f64>,
+    answer: Vec<(usize, usize)>,
+    score: i64, // スコア
+    rng: rand::rngs::ThreadRng,
+}
+
+impl State {
+    fn new(input: &Input) -> Self {
+        Self {
+            field: Self::build_field(input),
+            lives: vec![1.0],
+            answer: vec![],
+            score: 0,
+            rng: rand::thread_rng(),
+        }
+    }
+
+    fn build_field(input: &Input) -> FieldState {
+        let mut prob = vec![vec![0.0; N]; N];
+        for i in 0..N {
+            for j in 0..N {
+                if input.s[i][j] == '.' {
+                    prob[i][j] = 1.0 / (N * N - input.m) as f64;
+                }
+            }
+        }
+        FieldState { s: input.s.clone(), prob }
+    }
+
+    fn print_answer(&self) {
+        for &(i, j) in &self.answer {
+            println!("{} {}", i + 1, j + 1);
+        }
+    }
+}
+
+fn update_each_turn(input: &Input, state: &mut State, pos: (usize, usize)) {
+    // -----------------------------------------------------------------------------
+    // 城塞の更新
+    // -----------------------------------------------------------------------------
+    let mut next = vec![vec![0.0; N]; N];
+    let mut life = state.lives[state.lives.len() - 1];
+    for i in 0..N {
+        for j in 0..N {
+            if state.field.prob[i][j] == 0.0 {
+                continue;
+            }
+            for (di, dj) in [(!0, 0), (1, 0), (0, !0), (0, 1)] {
+                let mut i2 = i;
+                let mut j2 = j;
+                while i2 + di < N && j2 + dj < N && state.field.s[i2 + di][j2 + dj] == '.' {
+                    i2 += di;
+                    j2 += dj;
+                }
+                next[i2][j2] += state.field.prob[i][j] * 0.25;
+            }
+        }
+    }
+    state.field.prob = next;
+    let (bi, bj) = pos;
+    if state.field.s[bi][bj] == '#' {
+        panic!("({}, {}) is already blocked (turn {})", bi, bj, state.lives.len() - 1);
+    }
+    life -= state.field.prob[bi][bj];
+    state.lives.push(life);
+    state.field.prob[bi][bj] = 0.0;
+    state.field.s[bi][bj] = '#';
+
+    // -----------------------------------------------------------------------------
+    //
+    // -----------------------------------------------------------------------------
+    let ub = (N * N - input.m - 1) as f64;
+    let ret = state.lives.iter().sum::<f64>();
+    let score = (ret / ub * 1e6).round() as i64;
+
+    state.score = score;
+    state.answer.push((bi, bj));
+}
+
+// fn compute_score_details(input: &Input, out: &[(usize, usize)]) -> (i64, String, FieldState, Vec<f64>) {
+//     let mut s = input.s.clone();
+//     let mut crt = vec![vec![0.0; N]; N];
+//     for i in 0..N {
+//         for j in 0..N {
+//             if input.s[i][j] == '.' {
+//                 crt[i][j] = 1.0 / (N * N - input.m) as f64;
+//             }
+//         }
+//     }
+//     let mut ret = 0.0;
+//     let mut lives = vec![1.0];
+//     let mut life = 1.0;
+//     for t in 0.. {
+//         let mut next = vec![vec![0.0; N]; N];
+//         for i in 0..N {
+//             for j in 0..N {
+//                 if crt[i][j] == 0.0 {
+//                     continue;
+//                 }
+//                 for (di, dj) in [(!0, 0), (1, 0), (0, !0), (0, 1)] {
+//                     let mut i2 = i;
+//                     let mut j2 = j;
+//                     while i2 + di < N && j2 + dj < N && s[i2 + di][j2 + dj] == '.' {
+//                         i2 += di;
+//                         j2 += dj;
+//                     }
+//                     next[i2][j2] += crt[i][j] * 0.25;
+//                 }
+//             }
+//         }
+//         crt = next;
+//         if t >= out.len() {
+//             break;
+//         }
+//         let (bi, bj) = out[t];
+//         if s[bi][bj] == '#' {
+//             return (
+//                 0,
+//                 format!("({}, {}) is already blocked (turn {})", bi, bj, t),
+//                 FieldState { s, prob: crt },
+//                 lives,
+//             );
+//         }
+//         life -= crt[bi][bj];
+//         crt[bi][bj] = 0.0;
+//         s[bi][bj] = '#';
+//         ret += life;
+//         lives.push(life);
+//     }
+//     let ub = (N * N - input.m - 1) as f64;
+//     let score = (ret / ub * 1e6).round() as i64;
+//     let err = if out.len() < N * N - input.m {
+//         "Not finished".to_owned()
+//     } else {
+//         String::new()
+//     };
+//     (score, err, FieldState { s, prob: crt }, lives)
+// }
+
+fn solve(input: &Input, info: &mut Info, state: &mut State) {
+    let p = iproduct!(0..N, 0..N).filter(|&(i, j)| input.s[i][j] != '#').collect::<Vec<_>>();
+    for pos in p {
+        update_each_turn(input, state, pos);
+    }
+}
+
+fn main() {
+    let input = Input::from_stdin();
+    let mut info = Info::new();
+    let mut state = State::new(&input);
+
+    solve(&input, &mut info, &mut state);
+    state.print_answer();
+    eprintln!("Score = {}", state.score);
+}
